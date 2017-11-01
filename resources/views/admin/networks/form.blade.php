5ead9f3ad7ea8391f4028f8df2002d5bd295403c
ibrahimSmuhammad <mslm356@outlook.com>
2017-11-01 17:47:10 +0200

    after adding  add  ISP form&doneWithTheDesign

--- resources/views/admin/networks/form.blade.php
+++ resources/views/admin/networks/form.blade.php
@@ -1,32 +1,25 @@
-<div class="row">
-    <div class="loading-sub" style="display: none;">
-        <div class="progress">
-            <div class="indeterminate"></div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-content">
-            <span class="card-title">network Info</span>
-            <div class="row">
-
-
+<div class="panel-body">
+    
 
                 {{--======================name===================--}}
-                <div class="input-field col s6">
-                    {!!Form::text('name', null,array('class'=>'validate','id'=>'name'))!!}
-                    <label class="active" for="name">Name</label>
-                </div>
+                
 
 
+           <div class="form-group">
+                        <label class="control-label col-md-3">NetWork Name</label>
+                            <div class="col-md-6">
+                                <section class="form-group-vertical">
+                                    <div class="input-group input-group-icon">
+                                        <span class="input-group-addon">
+                                            <span class="icon"><i class="fa fa-user"></i></span>
+                                        </span>
+                    {!!Form::text('name', null,array('class'=>'validate form-control','id'=>'name'))!!}
+
+                                    </div>
+                                </section>
+                            </div>
             </div>
-        </div>
-    </div>
-
-    <div class="loading-sub" style="display: none;">
-        <div class="progress">
-            <div class="indeterminate"></div>
-        </div>
-    </div>
+<div class="col-md-12 text-center" >
+{!! Form::submit($submitButton, array('class'=>'btn btn-success text-center','id' => 'submit')) !!}
 </div>
-
-{!! Form::submit($submitButton, array('class'=>'btn btn-primary text-center','id' => 'submit')) !!}
+</div>
