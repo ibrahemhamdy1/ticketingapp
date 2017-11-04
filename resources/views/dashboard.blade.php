8fcbf61cd895aa33d7ab4d0f44d1c0af24f5021e
ibrhahim hamdy <mslm356@outlook.com>
2017-11-04 14:43:45 +0200

    start styling  the  dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,27 @@
 @extends('admin.main')
 @section('content')
+<div class="contianer" style="margin-top:20px">
+  <div class="row">
+      <div class="col-md-4 text-center">
+          <div class="panel-body bg-tertiary">
+            <div class="widget-summary">
+              <div class="widget-summary-col widget-summary-col-icon">
+                <div class="summary-icon">
+                  <i class="fa fa-life-ring"></i>
+                </div>
+              </div>
+              <div class="widget-summary-col text-center">
+                <div class="summary">
+                  <h4 class="title">Support Questions</h4>
+                  <div class="info">
+                    <strong class="amount">1281</strong>
+                  </div>
+                </div>
 
+              </div>
+            </div>
+          </div>
+      </div>
+  </div>
+</div>
 @endsection
